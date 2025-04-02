5ddb6aafaa4c89985abc2f74df4d17fc951b5ffe
Maks Kozub <maks.kozub@gmail.com>
2025-04-02 14:33:06 +0200

    first commit

--- obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4f1458046c20f07426eccfef633f34fd778258")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
